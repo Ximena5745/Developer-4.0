5d8c113453f3773bf0497b5b27f1a3d6d79c107d
Ximena5745 <ximena5745@gmail.com>
2021-12-18 23:24:23 -0500

    Creación de modelos

---
+++ Backend/Modelos/Avance/avances.js
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose';
+import { ProjectModel } from '../proyecto/proyecto.js';
+import { UserModel } from '../usuario/usuario.js';
+
+
+
+const {Schema, model}= mongoose
+
+const AdvanceProject =new Schema({
+    
+    project_id: {
+        type: String,
+        // required: true, 
+    },
+    date: {
+        type:Date,  
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    usuario: {
+        type: Schema.Types.ObjectId,
+        ref: UserModel,
+        required: true,
+    },
+    observaciones: {
+        type: String,
+        required: true,
+        },
+
+    proyecto: {
+        type: Schema.Types.ObjectId,
+        ref: ProjectModel,
+        required: true,
+      },
+
+    });
+    
+    const ModeloAvance = model('Avance', AdvanceProject);
+
+    export { ModeloAvance };
+    
